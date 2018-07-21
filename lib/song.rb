b143627a67280b49708d67539519276f51937d93
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-21 14:07:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,9 +28,10 @@ class Song
     @@genres.uniq 
   end 
   
-  def genre_count
+  def self.genre_count
     genre_count = {}
-    @@genres.each do |genre, songs|
+    @@genres.each do |genre|
+      if genre_count[genre]
       
     
   
